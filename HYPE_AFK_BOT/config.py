1f4779949b20f8278d4cf73f50c94ee7164bc1c3
HypeVoidSoul <87024922+HypeVoidSoul@users.noreply.github.com>
2021-07-08 09:57:22 -0700

    Update config.py

--- HYPE_AFK_BOT/config.py
+++ HYPE_AFK_BOT/config.py
@@ -3,12 +3,12 @@ import os
 
 class Config(object):
     LOGGER = True
-    API_ID =  6372795 
-    API_HASH = "4b7731b0a6d8e15bef82863887feb293"
-    TOKEN = "1825575975:AAESpVD0Q4spSGZmz1J1aDeeOpjRLGu0DcE"  
+    API_ID =  637
+    API_HASH = "4b7731b0"
+    TOKEN = "1825575975:AAESpVD"  
     OWNER_ID = 1836310130  
     OWNER_USERNAME = "@hypevoidsoul"
-    SSQLALCHEMY_DATABASE_URI = "postgresql://jttrtiadnflnpo:5f277b7428e3eb1d5faf750cc94b3d6101ad6791524ce740c34cdc59badba4d5@ec2-34-232-191-133.compute-1.amazonaws.com:5432/ddlot51iq9tl5k"
+    SSQLALCHEMY_DATABASE_URI = "postgresql:/"
     LOAD = []
     NO_LOAD = []
     WORKERS = 8
