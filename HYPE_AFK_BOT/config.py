9493fccd41387b11130985093dbac1f9e26682df
HypeVoidSoul <87024922+HypeVoidSoul@users.noreply.github.com>
2021-07-08 01:41:44 -0700

    Update config.py

--- HYPE_AFK_BOT/config.py
+++ HYPE_AFK_BOT/config.py
@@ -8,7 +8,7 @@ class Config(object):
     TOKEN = "1825575975:AAESpVD0Q4spSGZmz1J1aDeeOpjRLGu0DcE"  
     OWNER_ID = 1836310130  
     OWNER_USERNAME = "@hypevoidsoul"
-    SQLALCHEMY_DATABASE_URI = "postgresql://jttrtiadnflnpo:5f277b7428e3eb1d5faf750cc94b3d6101ad6791524ce740c34cdc59badba4d5@ec2-34-232-191-133.compute-1.amazonaws.com:5432/ddlot51iq9tl5k"
+    SSQLALCHEMY_DATABASE_URI = "postgresql://jttrtiadnflnpo:5f277b7428e3eb1d5faf750cc94b3d6101ad6791524ce740c34cdc59badba4d5@ec2-34-232-191-133.compute-1.amazonaws.com:5432/ddlot51iq9tl5k"
     LOAD = []
     NO_LOAD = []
     WORKERS = 8
