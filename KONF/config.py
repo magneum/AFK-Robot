4e7e76954512d47412f5d6385282a6bb5858a957
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-16 09:54:34 -0700

    Update config.py

--- KONF/config.py
+++ KONF/config.py
@@ -8,6 +8,6 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 class PIPER(Config):
     LOGGER = True
